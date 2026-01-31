96554be115831449f1c737f19339c2cd5c9d2d8b
0xHanniba1 <gzktmac1@gmail.com>
2026-01-31 12:40:23 +0800

    Site updated: 2026-01-31 12:40:23

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -1,7 +1,7 @@
 (function () {
   var lightSheet = document.getElementById('theme-light');
   var darkSheet = document.getElementById('theme-dark');
-  var toggleBtn = document.getElementById('theme-toggle');
+  var toggleBtn = document.getElementById('topbar-theme-toggle');
   if (!toggleBtn) return;
 
   var icon = toggleBtn.querySelector('i');
@@ -11,15 +11,11 @@
     if (theme === 'dark') {
       lightSheet.disabled = true;
       darkSheet.disabled = false;
-      icon.className = 'fa fa-sun-o';
-      icon.innerHTML = '&nbsp;';
-      toggleBtn.lastChild.textContent = 'Dark';
+      icon.className = 'fa fa-moon-o';
     } else {
       lightSheet.disabled = false;
       darkSheet.disabled = true;
-      icon.className = 'fa fa-moon-o';
-      icon.innerHTML = '&nbsp;';
-      toggleBtn.lastChild.textContent = 'Light';
+      icon.className = 'fa fa-sun-o';
     }
     localStorage.setItem('theme', theme);
     if (theme === 'dark') {
